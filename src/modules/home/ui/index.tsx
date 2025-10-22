b2423115efc79a75305f2f0d55cf4dc330f1a035
Vansh Mahajan <vanshmahajan2023@gmail.com>
2025-10-22 16:56:15 +0530

    hackerwrath section added

--- src/modules/home/ui/index.tsx
+++ src/modules/home/ui/index.tsx
@@ -1,5 +1,6 @@
 import EventsSection from "./sections/events";
 import HeroSection from "./sections/hero";
+import HackerwrathSection from "./sections/hackerwrath";
 import ShopSection from "./sections/shop";
 import SponsorsSection from "./sections/sponsors";
 
@@ -7,12 +8,9 @@ export default function LandingView() {
   return (
     <div className="flex flex-col">
       <HeroSection />
-      
-        <EventsSection />
-      
-      
-        <ShopSection />
-      
+      <HackerwrathSection />
+      <EventsSection />
+      <ShopSection />
       <div className="py-20">
         <SponsorsSection />
       </div>
